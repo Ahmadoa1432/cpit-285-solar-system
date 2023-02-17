3853eaa233a6d5e938e3db6b24eba5bfbe4bebce
Ahmadoa1432 <ahmedoa2000@gmail.com>
2023-02-17 21:56:49 +0300

    added the ability to modifi the sun size

--- py3d/project/solar-system.py
+++ py3d/project/solar-system.py
@@ -27,8 +27,6 @@ from py3d.core_ext.render_target import RenderTarget
 from py3d.core_ext.renderer import Renderer
 from py3d.core_ext.scene import Scene
 from py3d.core_ext.texture import Texture
-from py3d.geometry.box import BoxGeometry
-from py3d.geometry.rectangle import RectangleGeometry
 from py3d.geometry.sphere import SphereGeometry
 from py3d.material.surface import SurfaceMaterial
 from py3d.material.texture import TextureMaterial
@@ -44,28 +42,33 @@ class Example(Base):
         print("Initializing program...")
         self.renderer = Renderer()
         self.scene = Scene()
-        self.camera = Camera(aspect_ratio=800/600)
+        self.camera = Camera(aspect_ratio=800/600,)
         self.rig = MovementRig()
         self.rig.add(self.camera)
         self.scene.add(self.rig)
-        self.rig.set_position([0, 1, 4])
-        sky_geometry = SphereGeometry(radius=50)
+        self.SUN_SIZE = 3
+        self.rig.set_position([0, 1, 4*self.SUN_SIZE])
+        sky_geometry = SphereGeometry(radius=70)
         sky_material = TextureMaterial(texture=Texture(file_name="images/dark_sky.jpg"))
         sky = Mesh(sky_geometry, sky_material)
         self.scene.add(sky)
 
+
+        SUN_SIZE = self.SUN_SIZE
+        SUN_LIGHT = 0.05 /SUN_SIZE
+
         # The Sun's lighting effect
-        point_light1 = PointLight(color=[0.9, 0.9, 0], position=[1.5, 0, 1.5], attenuation=[0.1,0,0.1])
+        point_light1 = PointLight(color=[0.9, 0.9, 0], position=[1.5*SUN_SIZE, 0, 1.5*SUN_SIZE], attenuation=[SUN_LIGHT,0,SUN_LIGHT])
         self.scene.add(point_light1)
-        point_light2 = PointLight(color=[0.9, 0.9, 0], position=[-1.5, 0, 1.5], attenuation=[0.1,0,0.1])
+        point_light2 = PointLight(color=[0.9, 0.9, 0], position=[-1.5*SUN_SIZE, 0, 1.5*SUN_SIZE], attenuation=[SUN_LIGHT,0,SUN_LIGHT])
         self.scene.add(point_light2)
-        point_light3 = PointLight(color=[0.9, 0.9, 0], position=[-1.5, 0, -1.5], attenuation=[0.1,0,0.1])
+        point_light3 = PointLight(color=[0.9, 0.9, 0], position=[-1.5*SUN_SIZE, 0, -1.5*SUN_SIZE], attenuation=[SUN_LIGHT,0,SUN_LIGHT])
         self.scene.add(point_light3)
-        point_light4 = PointLight(color=[0.9, 0.9, 0], position=[1.5, 0, -1.5], attenuation=[0.1,0,0.1])
+        point_light4 = PointLight(color=[0.9, 0.9, 0], position=[1.5*SUN_SIZE, 0, -1.5*SUN_SIZE], attenuation=[SUN_LIGHT,0,SUN_LIGHT])
         self.scene.add(point_light4)
-        point_light5 = PointLight(color=[0.9, 0.9, 0], position=[0, 2,0], attenuation=[0.1,0,0.1])
+        point_light5 = PointLight(color=[0.9, 0.9, 0], position=[0, 2*SUN_SIZE,0], attenuation=[SUN_LIGHT,0,SUN_LIGHT])
         self.scene.add(point_light5)
-        point_light6 = PointLight(color=[0.9, 0.9, 0], position=[0, -2,0], attenuation=[0.1,0,0.1])
+        point_light6 = PointLight(color=[0.9, 0.9, 0], position=[0, -2*SUN_SIZE,0], attenuation=[SUN_LIGHT,0,SUN_LIGHT])
         self.scene.add(point_light6)
 
         # textures materials
@@ -88,7 +91,7 @@ class Example(Base):
         )
 
         #Sun Object
-        sun = SphereGeometry()
+        sun = SphereGeometry(radius=SUN_SIZE)
         self.sun = Mesh(sun, textured_phong_sun)
         self.sun.set_position([0, 0, 0])
         self.scene.add(self.sun)
@@ -96,35 +99,35 @@ class Example(Base):
         #Earth Object
         Earth = SphereGeometry()
         self.earth = Mesh(Earth, textured_flat_earth)
-        self.earth.set_position([8, 0, 0])
+        self.earth.set_position([5*SUN_SIZE, 0, 0])
         self.scene.add(self.earth)
 
-        # # Uncomment for glow effect
-        #
-        # self.postprocessor = Postprocessor(self.renderer, self.scene, self.camera)
-        #
-        # # glow scene.
-        # self.glow_scene = Scene()
-        # yellow_material = SurfaceMaterial(property_dict={"baseColor": [1, 1, 0]})
-        # glow_sphere = Mesh(sun, yellow_material)
-        # glow_sphere.local_matrix = self.sun.local_matrix
-        # self.glow_scene.add(glow_sphere)
-        #
-        # # glow postprocessing
-        # glow_target = RenderTarget(resolution=[800, 600])
-        # self.glow_pass = Postprocessor(self.renderer, self.glow_scene, self.camera, glow_target)
-        # self.glow_pass.add_effect(HorizontalBlurEffect(texture_size=[800, 600], blur_radius=50))
-        # self.glow_pass.add_effect(VerticalBlurEffect(texture_size=[800, 600], blur_radius=50))
-        #
-        # # combining results of glow effect with main scene
-        # self.combo_pass = Postprocessor(self.renderer, self.scene, self.camera)
-        # self.combo_pass.add_effect(
-        #     AdditiveBlendEffect(
-        #         blend_texture=glow_target.texture,
-        #         original_strength=1,
-        #         blend_strength=3
-        #     )
-        # )
+        # Uncomment for glow effect
+
+        self.postprocessor = Postprocessor(self.renderer, self.scene, self.camera)
+
+        # glow scene.
+        self.glow_scene = Scene()
+        yellow_material = SurfaceMaterial(property_dict={"baseColor": [1, 1, 0]})
+        glow_sphere = Mesh(sun, yellow_material)
+        glow_sphere.local_matrix = self.sun.local_matrix
+        self.glow_scene.add(glow_sphere)
+
+        # glow postprocessing
+        glow_target = RenderTarget(resolution=[800, 600])
+        self.glow_pass = Postprocessor(self.renderer, self.glow_scene, self.camera, glow_target)
+        self.glow_pass.add_effect(HorizontalBlurEffect(texture_size=[800, 600], blur_radius=50))
+        self.glow_pass.add_effect(VerticalBlurEffect(texture_size=[800, 600], blur_radius=50))
+
+        # combining results of glow effect with main scene
+        self.combo_pass = Postprocessor(self.renderer, self.scene, self.camera)
+        self.combo_pass.add_effect(
+            AdditiveBlendEffect(
+                blend_texture=glow_target.texture,
+                original_strength=1,
+                blend_strength=0.1*SUN_SIZE
+            )
+        )
 
 
     def update(self):
@@ -132,14 +135,14 @@ class Example(Base):
 
         # Earth rotation around itself and around the sun
         self.earth.translate(-0.01, 0, -0.08)
-        self.earth.rotate_y(0.01337)
+        self.earth.rotate_y(0.01337/self.SUN_SIZE)
 
         self.rig.update(self.input, self.delta_time)
         self.renderer.render(self.scene, self.camera)
 
-        ## Uncomment for glow effect
-        # self.glow_pass.render()
-        # self.combo_pass.render()
+        # Uncomment for glow effect
+        self.glow_pass.render()
+        self.combo_pass.render()
 
 # Instantiate this class and run the program
 Example(screen_size=[800, 600]).run()
